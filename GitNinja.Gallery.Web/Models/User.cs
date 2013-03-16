98e33279b18c588a944b1a1233ef8b68cf5eaf73
Rob Thijssen <rthijssen@gmail.com>
2013-03-16 18:06:15 +0000

    Handle non ldap situations

--- GitNinja.Gallery.Web/Models/User.cs
+++ GitNinja.Gallery.Web/Models/User.cs
@@ -1,36 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using GitNinja.Gallery.Web.Helpers;
 
 namespace GitNinja.Gallery.Web.Models
 {
-    public class User
+  public class User
+  {
+    public string Id { get; set; }
+
+    public string LdapPath { get; set; }
+
+    public string Email { get; set; }
+
+    public string Forename { get; set; }
+
+    public string Surname { get; set; }
+
+    public string DisplayName { get { return (!string.IsNullOrWhiteSpace(Forename) && !string.IsNullOrWhiteSpace(Surname)) ? string.Format("{0} {1}", Forename, Surname) : Id; } }
+
+    public static User Current
     {
-        public string Id { get; set; }
-
-        public string LdapPath { get; set; }
-
-        public string Email { get; set; }
-
-        public string Forename { get; set; }
-
-        public string Surname { get; set; }
-
-        public string DisplayName { get { return string.Format("{0} {1}", Forename, Surname); } }
-
-        public static User Current { get { return Ldap.GetUser(HttpContext.Current.User.Identity.Name); } }
+      get
+      {
+        User user;
+        try { user = Ldap.GetUser(HttpContext.Current.User.Identity.Name); }
+        catch { user = new User { Id = HttpContext.Current.User.Identity.Name }; }
+        return user;
+      }
     }
+  }
 
-    public class Group : List<User>
-    {
-        public string Id { get; set; }
+  public class Group : List<User>
+  {
+    public string Id { get; set; }
 
-        public string LdapPath { get; set; }
+    public string LdapPath { get; set; }
 
-        public DateTime? Created { get; set; }
+    public DateTime? Created { get; set; }
 
-        public string Isp { get; set; }
-    }
+    public string Isp { get; set; }
+  }
 }
