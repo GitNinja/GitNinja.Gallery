689e2691f690aa557aad5d83b9c723d80912f606
tjdecke <tom.joedecke@googlemail.com>
2013-03-22 12:50:44 +0000

    added breadcrumb display in tree browser

---
+++ GitNinja.Gallery.Web/Models/View/PathSegment.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GitNinja.Gallery.Web.Models.View
+{
+    public class PathSegment
+    {
+        public string Name { get; private set; }
+        public string Path { get; private set; }
+        public bool IsLast { get; private set; }
+
+        public static IEnumerable<PathSegment> FromPath(string path)
+        {
+            var segments = new List<PathSegment>();
+
+            if (string.IsNullOrEmpty(path))
+                return segments;
+
+            var splitPath = path.Split('/');
+            for (var i = 0; i < splitPath.Length; i++)
+            {
+                var segment = new PathSegment();
+                segment.Name = splitPath[i];
+                
+                var sb = new StringBuilder();
+                for (var j = 0; j <= i; j++)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append('/');
+                    }
+                    sb.Append(splitPath[j]);
+                }
+
+                if (i == (splitPath.Length-1))
+                {
+                    segment.IsLast = true;
+                }
+                segment.Path = sb.ToString();
+
+                segments.Add(segment);
+            }
+            return segments;
+        }
+    }
+}
